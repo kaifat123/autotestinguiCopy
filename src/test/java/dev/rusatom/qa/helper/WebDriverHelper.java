c718e44ca5c1cc61c9e64e8b77aa9a30e079d064
u17364669 <dark_said@mail.ru>
2020-10-08 21:43:42 +0300

    123

--- src/test/java/dev/rusatom/qa/helper/WebDriverHelper.java
+++ src/test/java/dev/rusatom/qa/helper/WebDriverHelper.java
@@ -4,26 +4,28 @@ import dev.rusatom.qa.factory.WebDriverFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverHelper {
     private static Logger logger = LogManager.getLogger(WebDriverHelper.class);
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
-            String browser = ConfProperties.getProperty("driver.browser.name");
-            browser = browser.toLowerCase();
-            driver = WebDriverFactory.create(browser);
+        String browser = ConfProperties.getProperty("driver.browser.name");
+        browser = browser.toLowerCase();
+        driver = WebDriverFactory.create(browser);
         ChromeOptions options = new ChromeOptions();
 
-options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
-            logger.info("Драйвер " + browser + " поднят");
+        logger.info("Драйвер " + browser + " поднят");
         return driver;
     }
 
     public static WebDriver getDriver(String value) {
-            driver = WebDriverFactory.create(value.toLowerCase());
-            logger.info("Драйвер " + value + " поднят");
+        driver = WebDriverFactory.create(value.toLowerCase());
+        logger.info("Драйвер " + value + " поднят");
         return driver;
     }
 }
